3f133cfccef6afe17904f6513d84c1cdff11daab
Rubendvb <rubenvasquez556@gmail.com>
2026-02-02 00:20:08 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,42 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseCurrentConnectionsResult = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity;",
+  );
+  const databaseCurrentConnectionsValue =
+    databaseCurrentConnectionsResult.rows[0].count;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    status: "ok",
-    message: "todos os endpoints estão funcionando corretamente",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        current_connections: parseInt(databaseCurrentConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectionsValue),
+      },
+    },
   });
 }
+
 export default status;
