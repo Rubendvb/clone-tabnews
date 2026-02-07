26116ad9b0949c4d900ec9e1d540b4d47f12f0f2
Rubendvb <rubenvasquez556@gmail.com>
2026-02-07 05:22:59 -0300

    handles database ssl connection

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,7 +8,7 @@ test("GET /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toBe("18.1");
+  expect(responseBody.dependencies.database.version).toBe("17.7");
   expect(responseBody.dependencies.database.max_connections).toBe(100);
   expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
